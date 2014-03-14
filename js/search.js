9f0884d4e53c5214e3aefa992cef0955cfe10d2f
jarrod <jclift@scrippsnetworks.com>
2014-03-14 13:23:39 -0400

    updated for interacting with Pedro's service

--- js/search.js
+++ js/search.js
@@ -1,25 +1,21 @@
 'use strict';
 
 angular.module('search', ['elasticsearch'])
-
-
  .service('es', function (esFactory) {
     return esFactory({
-      host: 'http://10.47.251.187:9200'
+      host: 'http://localhost:8080/myapp'
     });
   })
   .controller('searchController', function ($window, $scope, es) {
-      var pageNum = 15//request.param('page', 0);
-      var perPage = 15;//request.param('per_page', 15);
+
       var userQuery = $scope.searchText;//request.param('search_query');
-      var userId = 1; //request.session.userId;
+
 
       $scope.search = function () {
         console.log( "searched for " + $scope.searchText );
 
      es.search({
-           index: 'ats_index',
-           type: 'episode',
+
            body: {
              query: {
                match: {
@@ -36,47 +32,7 @@ angular.module('search', ['elasticsearch'])
          })
 
       };
-     /*
-        es.ping({
-                     requestTimeout: 1000,
-                     hello: "elasticsearch!"
 
-                   }, function (error) {
-                     if (error) {
-                       console.error('elasticsearch cluster is down!');
-                     } else {
-                       console.log('All is well');
-                     }
-                   })
-
-      */
-
-    /*es.search({
-                index: 'people_index',
-                from: (pageNum - 1) * perPage,
-                size: perPage,
-                body: {
-                    query: {
-                              match: {
-                                _all: 'Burton'
-                              }
-                            }
-
-                }
-              }, function (error, response) {
-
-                    if (error) {
-                      // handle error
-                      return;
-                    }
-
-                    response.render('search_results', {
-                      results: response.hits.hits,
-                      page: pageNum,
-                      pages: Math.ceil(response.hits.total / perPage)
-                    });
-             })
-             */
   });
 
 
